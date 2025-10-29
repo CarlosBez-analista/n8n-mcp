a532f42764faa29ac478eb999d2f28d129526e22
github-action-benchmark <github@users.noreply.github.com>
2025-10-29 15:41:29 +0000

    add n8n-mcp Benchmarks (customSmallerIsBetter) benchmark result for 3f427f952836fd35cb6d054a45d179d88b9cb37b

--- benchmarks/data.js
+++ benchmarks/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1758742667539,
+  "lastUpdate": 1761752489343,
   "repoUrl": "https://github.com/CarlosBez-analista/n8n-mcp",
   "entries": {
     "n8n-mcp Benchmarks": [
@@ -208,6 +208,37 @@ window.BENCHMARK_DATA = {
             "range": "0.4586",
             "unit": "ms",
             "extra": "14480 ops/sec"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "56956555+czlonkowski@users.noreply.github.com",
+            "name": "Romuald Członkowski",
+            "username": "czlonkowski"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "3f427f952836fd35cb6d054a45d179d88b9cb37b",
+          "message": "Update n8n to 1.117.2 (#379)",
+          "timestamp": "2025-10-28T08:55:20+01:00",
+          "tree_id": "19b592c4b1b26554b72fb4d46e5c2c87afac7aea",
+          "url": "https://github.com/CarlosBez-analista/n8n-mcp/commit/3f427f952836fd35cb6d054a45d179d88b9cb37b"
+        },
+        "date": 1761752488606,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "sample - array sorting - small",
+            "value": 0.0136,
+            "range": "0.3096",
+            "unit": "ms",
+            "extra": "73341 ops/sec"
           }
         ]
       }
